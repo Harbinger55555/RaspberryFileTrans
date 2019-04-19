b5222e433684c3cb49c5931371e55c07376161e4
Zaykha San <zaykha@zaykha-macbookpro.roam.corp.google.com>
2019-04-18 23:54:09 -0700

    Added request parsing functions.

--- library.py
+++ library.py
@@ -3,7 +3,10 @@ from __future__ import division
 from __future__ import print_function
 import socket
 import time
+
+
 COMMAND_BUFFER_SIZE = 256
+
 
 def CreateServerSocket(port):
     """Creates a socket that listens on a specified port.
@@ -19,10 +22,30 @@ def CreateServerSocket(port):
     server.listen()
     return server
 
+
 def ConnectClientToServer(server_sock):
 	return server_sock.accept()
+
 
 def CreateClientSocket(server_addr, port):
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	client.connect((server_addr, port))
 	return client
+
+
+def ReadRequest(sock):
+    """Read the request line from a socket. The request must end in newline."""
+    return sock.recv(COMMAND_BUFFER_SIZE).decode()
+
+
+def ParseRequest(request):
+    """Parses the request and returns the command name and filepath."""
+    args = command.strip().split(' ')
+    command = None
+    if args:
+        command = args[0]
+    filepath = None
+    if len(args) > 1:
+        filepath = ' '.join(args[1:])
+    return commmand, filepath
+
